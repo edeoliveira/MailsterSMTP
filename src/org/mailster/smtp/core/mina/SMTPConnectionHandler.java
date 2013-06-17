8353c42e7da5f70e31b251e8a07c7d0f9d7bb0a5
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2013-06-17 15:39:39 +0200

    Code fix

--- src/org/mailster/smtp/core/mina/SMTPConnectionHandler.java
+++ src/org/mailster/smtp/core/mina/SMTPConnectionHandler.java
@@ -116,7 +116,7 @@ public class SMTPConnectionHandler extends IoHandlerAdapter
 				sendResponse(session, "554 Transaction failed. Too many connections.");
 			}
 			else
-				sendResponse(session, "220 " + config.getHostName() + " ESMTP " + config.NAME);
+				sendResponse(session, "220 " + config.getHostName() + " ESMTP " + SMTPServerConfig.NAME);
 		}
 		catch (IOException e1)
 		{
